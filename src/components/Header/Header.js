0621f2d1d4812d1133aaeae033bc1dbf26c2b990
sarafat27 <sarafat2769@gmail.com>
2022-03-26 02:34:37 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Football Auction</h1>
+            <h2>Choose 4 footballers</h2>
+        </div>
+    );
+};
+
+export default Header;
